b7d2c892c4d93eeb6864165698da05583e25a5c5
Joey Wilhelm <jwilhelm@godaddy.com>
2019-04-26 14:22:15 -0400

    Minor cleanup/refactor of client instantiation

--- zoomus/client.py
+++ zoomus/client.py
@@ -7,26 +7,52 @@ from zoomus import (
     util)
 
 
+API_VERSION_1 = 1
+API_VERSION_2 = 2
+
+API_BASE_URIS = {
+    API_VERSION_1: 'https://api.zoom.us/v1',
+    API_VERSION_2: 'https://api.zoom.us/v2'
+}
+
+COMPONENT_CLASSES = {
+    API_VERSION_1: {
+        'user': components.user.UserComponent,
+        'meeting': components.meeting.MeetingComponent,
+        'report': components.report.ReportComponent,
+        'webinar': components.webinar.WebinarComponent,
+        'recording': components.recording.RecordingComponent,
+    },
+    API_VERSION_2: {
+        'user': components.user.UserComponentV2,
+        'meeting': components.meeting.MeetingComponentV2,
+        'report': components.report.ReportComponentV2,
+        'webinar': components.webinar.WebinarComponentV2,
+        'recording': components.recording.RecordingComponentV2,
+    }
+}
+
+
 class ZoomClient(util.ApiClient):
     """Zoom.us REST API Python Client"""
 
-    BASE_URI_V1 = 'https://api.zoom.us/v1'
-    BASE_URI_V2 = 'https://api.zoom.us/v2'
     """Base URL for Zoom API"""
 
-    def __init__(
-            self, api_key, api_secret, data_type='json', timeout=15, version=1):
+    def __init__(self, api_key, api_secret, data_type='json', timeout=15, version=API_VERSION_1):
         """Create a new Zoom client
 
         :param api_key: The Zooom.us API key
         :param api_secret: The Zoom.us API secret
         :param data_type: The expected return data type. Either 'json' or 'xml'
-        :param timeout: The time out to use for API requets
+        :param timeout: The time out to use for API requests
         """
-        BASE_URI = ZoomClient.BASE_URI_V1 if version == 1 else ZoomClient.BASE_URI_V2
+        try:
+            BASE_URI = API_BASE_URIS[version]
+            self.components = COMPONENT_CLASSES[version].copy()
+        except KeyError:
+            raise RuntimeError("API version not supported: %s" % version)
 
-        super(ZoomClient, self).__init__(
-            base_uri=BASE_URI, timeout=timeout)
+        super(ZoomClient, self).__init__(base_uri=BASE_URI, timeout=timeout)
 
         # Setup the config details
         self.config = {
@@ -37,25 +63,9 @@ class ZoomClient(util.ApiClient):
             'token': util.generate_jwt(api_key, api_secret),
         }
 
-        class_user_component = components.user.UserComponent if version == 1 else components.user.UserComponentV2
-        class_meeting_component = components.meeting.MeetingComponent if version == 1 else components.meeting.MeetingComponentV2
-        class_recording_component = components.recording.RecordingComponent if version == 1 else components.recording.RecordingComponentV2
-        class_webinar_component = components.webinar.WebinarComponent if version == 1 else components.webinar.WebinarComponentV2
-        class_report_component = components.report.ReportComponent if version == 1 else components.report.ReportComponentV2
-
-        # Register all of the components
-        self.components = {
-            'meeting': class_meeting_component(
-                base_uri=BASE_URI, config=self.config),
-            'report': class_report_component(
-                base_uri=BASE_URI, config=self.config),
-            'user': class_user_component(
-                base_uri=BASE_URI, config=self.config),
-            'webinar': class_webinar_component(
-                base_uri=BASE_URI, config=self.config),
-            'recording': class_recording_component(
-                base_uri=BASE_URI, config=self.config)
-        }
+        # Instantiate the components
+        for key in self.components.keys():
+            self.components[key] = self.components[key](base_uri=BASE_URI, config=self.config)
 
     def __enter__(self):
         return self
