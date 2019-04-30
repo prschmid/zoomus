f0f274c1c2bc0aa59311ab2fbb3d184f57029a47
Joey Wilhelm <tarkatronic@gmail.com>
2019-04-30 08:14:08 -0400

    Prepend URLs with a slash for consistency with V1

--- zoomus/components/meeting.py
+++ zoomus/components/meeting.py
@@ -45,29 +45,29 @@ class MeetingComponentV2(base.BaseComponent):
     def list(self, **kwargs):
         util.require_keys(kwargs, 'user_id')
         return self.get_request(
-            "users/{}/meetings".format(kwargs.get('user_id')),
+            "/users/{}/meetings".format(kwargs.get('user_id')),
             params=kwargs)
 
     def create(self, **kwargs):
         util.require_keys(kwargs, 'user_id')
         return self.post_request(
-            "users/{}/meetings".format(kwargs.get('user_id')),
+            "/users/{}/meetings".format(kwargs.get('user_id')),
             params=kwargs)
 
     def retrieve(self, **kwargs):
         util.require_keys(kwargs, 'id')
         return self.get_request(
-            "meetings/{}".format(kwargs.get('id')),
+            "/meetings/{}".format(kwargs.get('id')),
             params=kwargs)
 
     def update(self, **kwargs):
         util.require_keys(kwargs, 'id')
         return self.patch_request(
-            "meetings/{}".format(kwargs.get('id')),
+            "/meetings/{}".format(kwargs.get('id')),
             params=kwargs)
 
     def delete(self, **kwargs):
         util.require_keys(kwargs, 'id')
         return self.delete_request(
-            "meetings/{}".format(kwargs.get('id')),
+            "/meetings/{}".format(kwargs.get('id')),
             params=kwargs)
