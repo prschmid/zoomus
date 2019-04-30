e61e52b59a796adbbe64ab3cf79ed752246a1264
Joey Wilhelm <tarkatronic@gmail.com>
2019-04-30 08:14:37 -0400

    Finish up test coverage for meeting updates

--- tests/zoomus/components/meeting/test_update.py
+++ tests/zoomus/components/meeting/test_update.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import unittest
 
 from mock import patch
@@ -48,6 +49,48 @@ class UpdateTestCase(unittest.TestCase):
             self.component.update(id='ID')
             self.assertEqual(
                 context.exception.message, "'host_id' must be set")
+
+    @patch.object(components.base.BaseComponent, 'post_request', return_value=True)
+    def test_start_time_gets_transformed(self, mock_post_request):
+        self.component.update(id='ID', host_id='ID', start_time=datetime(1969, 1, 1))
+        mock_post_request.assert_called_with(
+            "/meeting/update",
+            params={
+                'id': 'ID',
+                'host_id': 'ID',
+                'start_time': '1969-01-01T00:00:00Z'
+            }
+        )
+
+
+class UpdateV2TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.component = components.meeting.MeetingComponentV2(
+            base_uri="http://foo.com",
+            config={
+                'api_key': 'KEY',
+                'api_secret': 'SECRET'
+            }
+        )
+
+    @patch.object(components.base.BaseComponent, 'patch_request', return_value=True)
+    def test_can_update(self, mock_post_request):
+        self.component.update(id='42', foo='bar')
+
+        mock_post_request.assert_called_with(
+            "/meetings/42",
+            params={
+                'id': '42',
+                'foo': 'bar'
+            }
+        )
+
+    def test_requires_id(self):
+        with self.assertRaises(ValueError) as context:
+            self.component.update()
+            self.assertEqual(
+                context.exception.message, "'id' must be set")
 
 
 if __name__ == '__main__':
