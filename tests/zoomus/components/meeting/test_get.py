65f836ba2036f440868f0e6c3e61e566b1f48827
Joey Wilhelm <jwilhelm@godaddy.com>
2019-05-03 15:25:47 -0400

    Cover meeting get for api v2

--- tests/zoomus/components/meeting/test_get.py
+++ tests/zoomus/components/meeting/test_get.py
@@ -23,7 +23,7 @@ class GetTestCase(unittest.TestCase):
             }
         )
 
-    def test_can_end(self):
+    def test_can_get(self):
         with patch.object(components.base.BaseComponent, 'post_request',
                           return_value=True) as mock_post_request:
 
@@ -48,6 +48,27 @@ class GetTestCase(unittest.TestCase):
             self.component.get(id='ID')
             self.assertEqual(
                 context.exception.message, "'host_id' must be set")
+
+
+class GetV2TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.component = components.meeting.MeetingComponentV2(
+            base_uri="http://foo.com",
+            config={
+                'api_key': 'KEY',
+                'api_secret': 'SECRET'
+            }
+        )
+
+    @patch.object(components.base.BaseComponent, 'get_request', return_value=True)
+    def test_can_get(self, mock_get_request):
+        self.component.get(id='ID')
+        mock_get_request.assert_called_with("/meetings/ID", params={'id': 'ID'})
+
+    def test_requires_id(self):
+        with self.assertRaisesRegexp(ValueError, "'id' must be set"):
+            self.component.get()
 
 
 if __name__ == '__main__':
