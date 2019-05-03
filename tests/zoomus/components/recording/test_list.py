b16e733cefa8dd1f5bb9a838ee5c2afc06d60cd3
Joey Wilhelm <jwilhelm@godaddy.com>
2019-05-03 15:53:22 -0400

    Cover listing recordings in api v2

--- tests/zoomus/components/recording/test_list.py
+++ tests/zoomus/components/recording/test_list.py
@@ -69,5 +69,51 @@ class ListTestCase(unittest.TestCase):
             )
 
 
+class ListV2TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.component = components.recording.RecordingComponentV2(
+            base_uri="http://foo.com",
+            config={
+                'api_key': 'KEY',
+                'api_secret': 'SECRET'
+            }
+        )
+
+    @patch.object(components.base.BaseComponent, 'get_request', return_value=True)
+    def test_can_list(self, mock_get_request):
+        self.component.list(user_id='ID')
+        mock_get_request.assert_called_with(
+            "/users/ID/recordings",
+            params={
+                'user_id': 'ID'
+            }
+        )
+
+    def test_requires_user_id(self):
+        with self.assertRaisesRegexp(ValueError, "'user_id' must be set"):
+            self.component.list()
+
+    @patch.object(components.base.BaseComponent, 'get_request', return_value=True)
+    def test_does_convert_startime_to_str_if_datetime(self, mock_get_request):
+        start_time = datetime.datetime.utcnow() - datetime.timedelta(days=10)
+        end_time = datetime.datetime.utcnow()
+        self.component.list(
+            user_id='ID',
+            start=start_time,
+            end=end_time,
+            meeting_number="111")
+
+        mock_get_request.assert_called_with(
+            "/users/ID/recordings",
+            params={
+                'user_id': 'ID',
+                'from': util.date_to_str(start_time),
+                'to': util.date_to_str(end_time),
+                "meeting_number": "111"
+            }
+        )
+
+
 if __name__ == '__main__':
     unittest.main()
