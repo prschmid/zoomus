224d9d2ee08ee6fffd5ae34b05247b79167a3d77
Joey Wilhelm <jwilhelm@godaddy.com>
2019-05-03 16:15:22 -0400

    Cover reports in api v2

--- tests/zoomus/components/report/test_get_account_report.py
+++ tests/zoomus/components/report/test_get_account_report.py
@@ -75,5 +75,56 @@ class GetAccountReportTestCase(unittest.TestCase):
             )
 
 
+class GetAccountReportV2TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.component = components.report.ReportComponentV2(
+            base_uri="http://foo.com",
+            config={
+                'api_key': 'KEY',
+                'api_secret': 'SECRET'
+            }
+        )
+
+    @patch.object(components.base.BaseComponent, 'get_request', return_value=True)
+    def test_can_get_account_report(self, mock_get_request):
+        start_time = datetime.datetime.utcnow()
+        end_time = datetime.datetime.utcnow()
+        self.component.get_account_report(
+            start_time=start_time, end_time=end_time)
+
+        mock_get_request.assert_called_with(
+            "/report/users",
+            params={
+                'from': util.date_to_str(start_time),
+                'to': util.date_to_str(end_time)
+            }
+        )
+
+    def test_requires_start_time(self):
+        with self.assertRaisesRegexp(ValueError, "'start_time' must be set"):
+            self.component.get_account_report()
+
+    def test_requires_end_time(self):
+        with self.assertRaisesRegexp(ValueError, "'end_time' must be set"):
+            self.component.get_account_report(start_time=datetime.datetime.utcnow())
+
+    @patch.object(components.base.BaseComponent, 'get_request', return_value=True)
+    def test_does_convert_start_time_to_str_if_datetime(self, mock_get_request):
+        start_time = datetime.datetime.utcnow()
+        end_time = datetime.datetime.utcnow()
+
+        self.component.get_account_report(
+            start_time=start_time, end_time=end_time)
+
+        mock_get_request.assert_called_with(
+            "/report/users",
+            params={
+                'from': util.date_to_str(start_time),
+                'to': util.date_to_str(end_time)
+            }
+        )
+
+
 if __name__ == '__main__':
     unittest.main()
--- tests/zoomus/components/report/test_get_user_report.py
+++ tests/zoomus/components/report/test_get_user_report.py
@@ -75,5 +75,62 @@ class GetUserReportTestCase(unittest.TestCase):
             )
 
 
+class GetUserReportV2TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.component = components.report.ReportComponentV2(
+            base_uri="http://foo.com",
+            config={
+                'api_key': 'KEY',
+                'api_secret': 'SECRET'
+            }
+        )
+
+    @patch.object(components.base.BaseComponent, 'get_request', return_value=True)
+    def test_can_get_user_report(self, mock_get_request):
+        start_time = datetime.datetime.utcnow()
+        end_time = datetime.datetime.utcnow()
+        self.component.get_user_report(
+            user_id='ID', start_time=start_time, end_time=end_time)
+
+        mock_get_request.assert_called_with(
+            "/report/users/ID/meetings",
+            params={
+                'user_id': 'ID',
+                'from': util.date_to_str(start_time),
+                'to': util.date_to_str(end_time)
+            }
+        )
+
+    def test_requires_user_id(self):
+        with self.assertRaisesRegexp(ValueError, "'user_id' must be set"):
+            self.component.get_user_report()
+
+    def test_requires_start_time(self):
+        with self.assertRaisesRegexp(ValueError, "'start_time' must be set"):
+            self.component.get_user_report(user_id='ID')
+
+    def test_requires_end_time(self):
+        with self.assertRaisesRegexp(ValueError, "'end_time' must be set"):
+            self.component.get_user_report(user_id='ID', start_time=datetime.datetime.utcnow())
+
+    @patch.object(components.base.BaseComponent, 'get_request', return_value=True)
+    def test_does_convert_start_time_to_str_if_datetime(self, mock_get_request):
+        start_time = datetime.datetime.utcnow()
+        end_time = datetime.datetime.utcnow()
+
+        self.component.get_user_report(
+            user_id='ID', start_time=start_time, end_time=end_time)
+
+        mock_get_request.assert_called_with(
+            "/report/users/ID/meetings",
+            params={
+                'user_id': 'ID',
+                'from': util.date_to_str(start_time),
+                'to': util.date_to_str(end_time)
+            }
+        )
+
+
 if __name__ == '__main__':
     unittest.main()
