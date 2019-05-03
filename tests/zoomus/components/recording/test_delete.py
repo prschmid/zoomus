87b7a1cb9ece6e495bc00426ed574f0b5c5addba
Joey Wilhelm <jwilhelm@godaddy.com>
2019-05-03 15:42:47 -0400

    Cover deleting recordings in api v2

--- tests/zoomus/components/recording/test_delete.py
+++ tests/zoomus/components/recording/test_delete.py
@@ -43,5 +43,31 @@ class DeleteTestCase(unittest.TestCase):
                 context.exception.message, "'meeting_id' must be set")
 
 
+class DeleteV2TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.component = components.recording.RecordingComponentV2(
+            base_uri="http://foo.com",
+            config={
+                'api_key': 'KEY',
+                'api_secret': 'SECRET'
+            }
+        )
+
+    @patch.object(components.base.BaseComponent, 'delete_request', return_value=True)
+    def test_can_delete(self, mock_delete_request):
+        self.component.delete(meeting_id='ID')
+        mock_delete_request.assert_called_with(
+            "/meetings/ID/recordings",
+            params={
+                'meeting_id': 'ID'
+            }
+        )
+
+    def test_requires_id(self):
+        with self.assertRaisesRegexp(ValueError, "'meeting_id' must be set"):
+            self.component.delete()
+
+
 if __name__ == '__main__':
     unittest.main()
