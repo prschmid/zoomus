07e7eb2068a1415ac9787079d8f3b1d87d729da0
Joey Wilhelm <jwilhelm@godaddy.com>
2019-05-03 15:32:00 -0400

    Cover deleting meetings in api v2

--- tests/zoomus/components/meeting/test_delete.py
+++ tests/zoomus/components/meeting/test_delete.py
@@ -50,5 +50,31 @@ class DeleteTestCase(unittest.TestCase):
                 context.exception.message, "'host_id' must be set")
 
 
+class DeleteV2TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.component = components.meeting.MeetingComponentV2(
+            base_uri="http://foo.com",
+            config={
+                'api_key': 'KEY',
+                'api_secret': 'SECRET'
+            }
+        )
+
+    @patch.object(components.base.BaseComponent, 'delete_request', return_value=True)
+    def test_can_delete(self, mock_delete_request):
+        self.component.delete(id='ID')
+        mock_delete_request.assert_called_with(
+            "/meetings/ID",
+            params={
+                'id': 'ID',
+            }
+        )
+
+    def test_requires_id(self):
+        with self.assertRaisesRegexp(ValueError, "'id' must be set"):
+            self.component.delete()
+
+
 if __name__ == '__main__':
     unittest.main()
