cde107066f6433ccc44e53474163fb9fef5ccdd0
Joey Wilhelm <jwilhelm@godaddy.com>
2019-05-03 15:16:28 -0400

    Cover listing meetings with api v2

--- tests/zoomus/components/meeting/test_list.py
+++ tests/zoomus/components/meeting/test_list.py
@@ -66,5 +66,32 @@ class ListTestCase(unittest.TestCase):
             )
 
 
+class ListV2TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.component = components.meeting.MeetingComponentV2(
+            base_uri="http://foo.com",
+            config={
+                'api_key': 'KEY',
+                'api_secret': 'SECRET'
+            }
+        )
+
+    @patch.object(components.base.BaseComponent, 'get_request', return_value=True)
+    def test_can_list(self, mock_get_request):
+        self.component.list(user_id='ID')
+
+        mock_get_request.assert_called_with(
+            "/users/ID/meetings",
+            params={
+                'user_id': 'ID'
+            }
+        )
+
+    def test_requires_user_id(self):
+        with self.assertRaisesRegexp(ValueError, "'user_id' must be set"):
+            self.component.list()
+
+
 if __name__ == '__main__':
     unittest.main()
