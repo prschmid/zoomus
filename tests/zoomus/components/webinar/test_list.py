caf480aaedaec7d2f3eed4fac5b287f0b5e22c4c
Joey Wilhelm <jwilhelm@godaddy.com>
2019-05-04 08:57:09 -0400

    Improving tests coverage on webinars...

--- tests/zoomus/components/webinar/test_list.py
+++ tests/zoomus/components/webinar/test_list.py
@@ -26,44 +26,65 @@ class ListTestCase(unittest.TestCase):
             }
         )
 
-    def test_can_list(self):
-        with patch.object(components.base.BaseComponent, 'post_request',
-                          return_value=True) as mock_post_request:
+    @patch.object(components.base.BaseComponent, 'post_request', return_value=True)
+    def test_can_list(self, mock_post_request):
+        self.component.list(host_id='ID')
 
-            self.component.list(host_id='ID')
-
-            mock_post_request.assert_called_with(
-                "/webinar/list",
-                params={
-                    'host_id': 'ID'
-                }
-            )
+        mock_post_request.assert_called_with(
+            "/webinar/list",
+            params={
+                'host_id': 'ID'
+            }
+        )
 
     def test_requires_host_id(self):
-        with self.assertRaises(ValueError) as context:
+        with self.assertRaisesRegexp(ValueError, "'host_id' must be set"):
             self.component.list()
-            self.assertEqual(
-                context.exception.message, "'host_id' must be set")
 
-    def test_does_convert_startime_to_str_if_datetime(self):
+    @patch.object(components.base.BaseComponent, 'post_request', return_value=True)
+    def test_does_convert_startime_to_str_if_datetime(self, mock_post_request):
+        start_time = datetime.datetime.utcnow()
+        self.component.list(
+            host_id='ID', topic='TOPIC', type='TYPE',
+            start_time=start_time)
 
-        with patch.object(components.base.BaseComponent, 'post_request',
-                          return_value=True) as mock_post_request:
+        mock_post_request.assert_called_with(
+            "/webinar/list",
+            params={
+                'host_id': 'ID',
+                'topic': 'TOPIC',
+                'type': 'TYPE',
+                'start_time': util.date_to_str(start_time)
+            }
+        )
 
-            start_time = datetime.datetime.utcnow()
-            self.component.list(
-                host_id='ID', topic='TOPIC', type='TYPE',
-                start_time=start_time)
 
-            mock_post_request.assert_called_with(
-                "/webinar/list",
-                params={
-                    'host_id': 'ID',
-                    'topic': 'TOPIC',
-                    'type': 'TYPE',
-                    'start_time': util.date_to_str(start_time)
-                }
-            )
+class ListV2TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.component = components.webinar.WebinarComponentV2(
+            base_uri="http://foo.com",
+            config={
+                'api_key': 'KEY',
+                'api_secret': 'SECRET'
+            }
+        )
+
+    @patch.object(components.base.BaseComponent, 'get_request', return_value=True)
+    def test_can_list(self, mock_get_request):
+        self.component.list(user_id='ID')
+
+        mock_get_request.assert_called_with(
+            "/users/ID/webinars",
+            params={
+                'user_id': 'ID'
+            }
+        )
+
+    def test_requires_user_id(self):
+        with self.assertRaisesRegexp(ValueError, "'user_id' must be set"):
+            self.component.list()
+
 
 
 if __name__ == '__main__':
--- tests/zoomus/components/webinar/test_update.py
+++ tests/zoomus/components/webinar/test_update.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import unittest
 
 from mock import patch
@@ -23,31 +24,37 @@ class UpdateTestCase(unittest.TestCase):
             }
         )
 
-    def test_can_update(self):
-        with patch.object(components.base.BaseComponent, 'post_request',
-                          return_value=True) as mock_post_request:
+    @patch.object(components.base.BaseComponent, 'post_request', return_value=True)
+    def test_can_update(self, mock_post_request):
+        self.component.update(id='ID', host_id='ID')
 
-            self.component.update(id='ID', host_id='ID')
-
-            mock_post_request.assert_called_with(
-                "/webinar/update",
-                params={
-                    'id': 'ID',
-                    'host_id': 'ID'
-                }
-            )
+        mock_post_request.assert_called_with(
+            "/webinar/update",
+            params={
+                'id': 'ID',
+                'host_id': 'ID'
+            }
+        )
 
     def test_requires_id(self):
-        with self.assertRaises(ValueError) as context:
+        with self.assertRaisesRegexp(ValueError, "'id' must be set"):
             self.component.update()
-            self.assertEqual(
-                context.exception.message, "'id' must be set")
 
     def test_requires_host_id(self):
-        with self.assertRaises(ValueError) as context:
+        with self.assertRaisesRegexp(ValueError, "'host_id' must be set"):
             self.component.update(id='ID')
-            self.assertEqual(
-                context.exception.message, "'host_id' must be set")
+
+    @patch.object(components.base.BaseComponent, 'post_request', return_value=True)
+    def test_start_time_gets_transformed(self, mock_post_request):
+        self.component.update(id='42', host_id='HOST', start_time=datetime(1969, 1, 1))
+        mock_post_request.assert_called_with(
+            "/webinar/update",
+            params={
+                'id': '42',
+                'host_id': 'HOST',
+                'start_time': '1969-01-01T00:00:00Z',
+            }
+        )
 
 
 if __name__ == '__main__':
