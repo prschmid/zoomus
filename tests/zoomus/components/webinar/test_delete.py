46ffa188a21cd301a6e0c1f7def4422d25f6dbcd
Joey Wilhelm <jwilhelm@godaddy.com>
2019-05-04 09:13:34 -0400

    Complete the test coverage of webinars

--- tests/zoomus/components/webinar/test_delete.py
+++ tests/zoomus/components/webinar/test_delete.py
@@ -23,31 +23,53 @@ class DeleteTestCase(unittest.TestCase):
             }
         )
 
-    def test_can_delete(self):
-        with patch.object(components.base.BaseComponent, 'post_request',
-                          return_value=True) as mock_post_request:
+    @patch.object(components.base.BaseComponent, 'post_request', return_value=True)
+    def test_can_delete(self, mock_post_request):
+        self.component.delete(id='ID', host_id='ID')
 
-            self.component.delete(id='ID', host_id='ID')
-
-            mock_post_request.assert_called_with(
-                "/webinar/delete",
-                params={
-                    'id': 'ID',
-                    'host_id': 'ID'
-                }
-            )
+        mock_post_request.assert_called_with(
+            "/webinar/delete",
+            params={
+                'id': 'ID',
+                'host_id': 'ID'
+            }
+        )
 
     def test_requires_id(self):
-        with self.assertRaises(ValueError) as context:
+        with self.assertRaisesRegexp(ValueError, "'id' must be set"):
             self.component.delete()
-            self.assertEqual(
-                context.exception.message, "'id' must be set")
 
     def test_requires_host_id(self):
-        with self.assertRaises(ValueError) as context:
+        with self.assertRaisesRegexp(ValueError, "'host_id' must be set"):
             self.component.delete(id='ID')
-            self.assertEqual(
-                context.exception.message, "'host_id' must be set")
+
+
+class DeleteV2TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.component = components.webinar.WebinarComponentV2(
+            base_uri="http://foo.com",
+            config={
+                'api_key': 'KEY',
+                'api_secret': 'SECRET'
+            }
+        )
+
+    @patch.object(components.base.BaseComponent, 'delete_request', return_value=True)
+    def test_can_delete(self, mock_delete_request):
+        self.component.delete(id='ID')
+
+        mock_delete_request.assert_called_with(
+            "/webinars/ID",
+            params={
+                'id': 'ID',
+            }
+        )
+
+    def test_requires_id(self):
+        with self.assertRaisesRegexp(ValueError, "'id' must be set"):
+            self.component.delete()
+
 
 
 if __name__ == '__main__':
--- tests/zoomus/components/webinar/test_end.py
+++ tests/zoomus/components/webinar/test_end.py
@@ -12,7 +12,7 @@ def suite():
     return suite
 
 
-class UpdateTestCase(unittest.TestCase):
+class EndTestCase(unittest.TestCase):
 
     def setUp(self):
         self.component = components.webinar.WebinarComponent(
@@ -23,31 +23,53 @@ class UpdateTestCase(unittest.TestCase):
             }
         )
 
-    def test_can_end(self):
-        with patch.object(components.base.BaseComponent, 'post_request',
-                          return_value=True) as mock_post_request:
+    @patch.object(components.base.BaseComponent, 'post_request', return_value=True)
+    def test_can_end(self, mock_post_request):
+        self.component.end(id='ID', host_id='ID')
 
-            self.component.end(id='ID', host_id='ID')
-
-            mock_post_request.assert_called_with(
-                "/webinar/end",
-                params={
-                    'id': 'ID',
-                    'host_id': 'ID'
-                }
-            )
+        mock_post_request.assert_called_with(
+            "/webinar/end",
+            params={
+                'id': 'ID',
+                'host_id': 'ID'
+            }
+        )
 
     def test_requires_id(self):
-        with self.assertRaises(ValueError) as context:
+        with self.assertRaisesRegexp(ValueError, "'id' must be set"):
             self.component.end()
-            self.assertEqual(
-                context.exception.message, "'id' must be set")
 
     def test_requires_host_id(self):
-        with self.assertRaises(ValueError) as context:
+        with self.assertRaisesRegexp(ValueError, "'host_id' must be set"):
             self.component.end(id='ID')
-            self.assertEqual(
-                context.exception.message, "'host_id' must be set")
+
+
+class EndV2TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.component = components.webinar.WebinarComponentV2(
+            base_uri="http://foo.com",
+            config={
+                'api_key': 'KEY',
+                'api_secret': 'SECRET'
+            }
+        )
+
+    @patch.object(components.base.BaseComponent, 'put_request', return_value=True)
+    def test_can_end(self, mock_put_request):
+        self.component.end(id='ID')
+
+        mock_put_request.assert_called_with(
+            "/webinars/ID/status",
+            params={
+                'status': 'end'
+            }
+        )
+
+    def test_requires_id(self):
+        with self.assertRaisesRegexp(ValueError, "'id' must be set"):
+            self.component.end()
+
 
 
 if __name__ == '__main__':
