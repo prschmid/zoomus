fab457a50ac315fcdf849d630e11b79585394681
Joey Wilhelm <jwilhelm@godaddy.com>
2019-10-01 14:04:33 -0400

    Switch meeting tests to use responses

--- tests/zoomus/components/meeting/test_list.py
+++ tests/zoomus/components/meeting/test_list.py
@@ -1,12 +1,8 @@
 import datetime
 import unittest
 
-try:
-    from unittest.mock import patch
-except ImportError:
-    from mock import patch
-
 from zoomus import components, util
+import responses
 
 
 def suite():
@@ -19,60 +15,54 @@ def suite():
 class ListV1TestCase(unittest.TestCase):
     def setUp(self):
         self.component = components.meeting.MeetingComponent(
-            base_uri="http://foo.com", config={"api_key": "KEY", "api_secret": "SECRET"}
+            base_uri="http://foo.com",
+            config={
+                "api_key": "KEY",
+                "api_secret": "SECRET",
+                "version": util.API_VERSION_1,
+            },
         )
 
+    @responses.activate
     def test_can_list(self):
-        with patch.object(
-            components.base.BaseComponent, "post_request", return_value=True
-        ) as mock_post_request:
-
-            self.component.list(host_id="ID")
-
-            mock_post_request.assert_called_with(
-                "/meeting/list", params={"host_id": "ID"}
-            )
+        responses.add(
+            responses.POST,
+            "http://foo.com/meeting/list?host_id=ID&api_key=KEY&api_secret=SECRET",
+        )
+        self.component.list(host_id="ID")
 
     def test_requires_host_id(self):
-        with self.assertRaises(ValueError) as context:
+        with self.assertRaisesRegexp(ValueError, "'host_id' must be set"):
             self.component.list()
-            self.assertEqual(context.exception.message, "'host_id' must be set")
 
+    @responses.activate
     def test_does_convert_startime_to_str_if_datetime(self):
-
-        with patch.object(
-            components.base.BaseComponent, "post_request", return_value=True
-        ) as mock_post_request:
-
-            start_time = datetime.datetime.utcnow()
-            self.component.list(
-                host_id="ID", topic="TOPIC", type="TYPE", start_time=start_time
-            )
-
-            mock_post_request.assert_called_with(
-                "/meeting/list",
-                params={
-                    "host_id": "ID",
-                    "topic": "TOPIC",
-                    "type": "TYPE",
-                    "start_time": util.date_to_str(start_time),
-                },
-            )
+        responses.add(
+            responses.POST,
+            "http://foo.com/meeting/list?host_id=ID&topic=TOPIC&type=TYPE&start_time=2020-01-01T01%3A01%3A00Z"
+            "&api_key=KEY&api_secret=SECRET",
+        )
+        start_time = datetime.datetime(2020, 1, 1, 1, 1)
+        self.component.list(
+            host_id="ID", topic="TOPIC", type="TYPE", start_time=start_time
+        )
 
 
 class ListV2TestCase(unittest.TestCase):
     def setUp(self):
         self.component = components.meeting.MeetingComponentV2(
-            base_uri="http://foo.com", config={"api_key": "KEY", "api_secret": "SECRET"}
+            base_uri="http://foo.com",
+            config={
+                "api_key": "KEY",
+                "api_secret": "SECRET",
+                "version": util.API_VERSION_2,
+            },
         )
 
-    @patch.object(components.base.BaseComponent, "get_request", return_value=True)
-    def test_can_list(self, mock_get_request):
+    @responses.activate
+    def test_can_list(self):
+        responses.add(responses.GET, "http://foo.com/users/ID/meetings?user_id=ID")
         self.component.list(user_id="ID")
-
-        mock_get_request.assert_called_with(
-            "/users/ID/meetings", params={"user_id": "ID"}
-        )
 
     def test_requires_user_id(self):
         with self.assertRaisesRegexp(ValueError, "'user_id' must be set"):
