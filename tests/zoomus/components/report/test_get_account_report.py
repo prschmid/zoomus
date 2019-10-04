d71b7f449ae56b465c510b726eab8c7f18f5836e
Joey Wilhelm <jwilhelm@godaddy.com>
2019-10-04 13:53:55 -0400

    Switch report tests to use responses

--- tests/zoomus/components/report/test_get_account_report.py
+++ tests/zoomus/components/report/test_get_account_report.py
@@ -1,12 +1,8 @@
 import datetime
 import unittest
 
-try:
-    from unittest.mock import patch
-except ImportError:
-    from mock import patch
-
 from zoomus import components, util
+import responses
 
 
 def suite():
@@ -19,75 +15,24 @@ def suite():
 class GetAccountReportV1TestCase(unittest.TestCase):
     def setUp(self):
         self.component = components.report.ReportComponent(
-            base_uri="http://foo.com", config={"api_key": "KEY", "api_secret": "SECRET"}
-        )
-
-    def test_can_get_account_report(self):
-        with patch.object(
-            components.base.BaseComponent, "post_request", return_value=True
-        ) as mock_post_request:
-
-            start_time = datetime.datetime.utcnow()
-            end_time = datetime.datetime.utcnow()
-            self.component.get_account_report(start_time=start_time, end_time=end_time)
-
-            mock_post_request.assert_called_with(
-                "/report/getaccountreport",
-                params={
-                    "from": util.date_to_str(start_time),
-                    "to": util.date_to_str(end_time),
-                },
-            )
-
-    def test_requires_start_time(self):
-        with self.assertRaises(ValueError) as context:
-            self.component.get_account_report()
-            self.assertEqual(context.exception.message, "'start_time' must be set")
-
-    def test_requires_end_time(self):
-        with self.assertRaises(ValueError) as context:
-            self.component.get_account_report(start_time=datetime.datetime.utcnow())
-            self.assertEqual(context.exception.message, "'end_time' must be set")
-
-    def test_does_convert_start_time_to_str_if_datetime(self):
-
-        with patch.object(
-            components.base.BaseComponent, "post_request", return_value=True
-        ) as mock_post_request:
-
-            start_time = datetime.datetime.utcnow()
-            end_time = datetime.datetime.utcnow()
-
-            self.component.get_account_report(start_time=start_time, end_time=end_time)
-
-            mock_post_request.assert_called_with(
-                "/report/getaccountreport",
-                params={
-                    "from": util.date_to_str(start_time),
-                    "to": util.date_to_str(end_time),
-                },
-            )
-
-
-class GetAccountReportV2TestCase(unittest.TestCase):
-    def setUp(self):
-        self.component = components.report.ReportComponentV2(
-            base_uri="http://foo.com", config={"api_key": "KEY", "api_secret": "SECRET"}
-        )
-
-    @patch.object(components.base.BaseComponent, "get_request", return_value=True)
-    def test_can_get_account_report(self, mock_get_request):
-        start_time = datetime.datetime.utcnow()
-        end_time = datetime.datetime.utcnow()
-        self.component.get_account_report(start_time=start_time, end_time=end_time)
-
-        mock_get_request.assert_called_with(
-            "/report/users",
-            params={
-                "from": util.date_to_str(start_time),
-                "to": util.date_to_str(end_time),
+            base_uri="http://foo.com",
+            config={
+                "api_key": "KEY",
+                "api_secret": "SECRET",
+                "version": util.API_VERSION_1,
             },
         )
+
+    @responses.activate
+    def test_can_get_account_report(self):
+        start_time = datetime.datetime(1969, 1, 1, 1, 1)
+        end_time = datetime.datetime(2020, 1, 1, 1, 1)
+        responses.add(
+            responses.POST,
+            "http://foo.com/report/getaccountreport?from=1969-01-01T01%3A01%3A00Z&to=2020-01-01T01%3A01%3A00Z"
+            "&api_key=KEY&api_secret=SECRET",
+        )
+        self.component.get_account_report(start_time=start_time, end_time=end_time)
 
     def test_requires_start_time(self):
         with self.assertRaisesRegexp(ValueError, "'start_time' must be set"):
@@ -97,20 +42,35 @@ class GetAccountReportV2TestCase(unittest.TestCase):
         with self.assertRaisesRegexp(ValueError, "'end_time' must be set"):
             self.component.get_account_report(start_time=datetime.datetime.utcnow())
 
-    @patch.object(components.base.BaseComponent, "get_request", return_value=True)
-    def test_does_convert_start_time_to_str_if_datetime(self, mock_get_request):
-        start_time = datetime.datetime.utcnow()
-        end_time = datetime.datetime.utcnow()
 
-        self.component.get_account_report(start_time=start_time, end_time=end_time)
-
-        mock_get_request.assert_called_with(
-            "/report/users",
-            params={
-                "from": util.date_to_str(start_time),
-                "to": util.date_to_str(end_time),
+class GetAccountReportV2TestCase(unittest.TestCase):
+    def setUp(self):
+        self.component = components.report.ReportComponentV2(
+            base_uri="http://foo.com",
+            config={
+                "api_key": "KEY",
+                "api_secret": "SECRET",
+                "version": util.API_VERSION_2,
             },
         )
+
+    @responses.activate
+    def test_can_get_account_report(self):
+        start_time = datetime.datetime(1969, 1, 1, 1, 1)
+        end_time = datetime.datetime(2020, 1, 1, 1, 1)
+        responses.add(
+            responses.GET,
+            "http://foo.com/report/users?from=1969-01-01T01%3A01%3A00Z&to=2020-01-01T01%3A01%3A00Z",
+        )
+        self.component.get_account_report(start_time=start_time, end_time=end_time)
+
+    def test_requires_start_time(self):
+        with self.assertRaisesRegexp(ValueError, "'start_time' must be set"):
+            self.component.get_account_report()
+
+    def test_requires_end_time(self):
+        with self.assertRaisesRegexp(ValueError, "'end_time' must be set"):
+            self.component.get_account_report(start_time=datetime.datetime.utcnow())
 
 
 if __name__ == "__main__":
