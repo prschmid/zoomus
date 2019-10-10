f4cb358d81674e23c8ca06ec531102750d3773ee
Joey Wilhelm <jwilhelm@godaddy.com>
2019-10-10 12:14:17 -0400

    Switch webinar tests to use responses

--- tests/zoomus/components/webinar/test_delete.py
+++ tests/zoomus/components/webinar/test_delete.py
@@ -1,11 +1,7 @@
 import unittest
 
-try:
-    from unittest.mock import patch
-except ImportError:
-    from mock import patch
-
-from zoomus import components
+from zoomus import components, util
+import responses
 
 
 def suite():
@@ -18,16 +14,21 @@ def suite():
 class DeleteV1TestCase(unittest.TestCase):
     def setUp(self):
         self.component = components.webinar.WebinarComponent(
-            base_uri="http://foo.com", config={"api_key": "KEY", "api_secret": "SECRET"}
+            base_uri="http://foo.com",
+            config={
+                "api_key": "KEY",
+                "api_secret": "SECRET",
+                "version": util.API_VERSION_1,
+            },
         )
 
-    @patch.object(components.base.BaseComponent, "post_request", return_value=True)
-    def test_can_delete(self, mock_post_request):
+    @responses.activate
+    def test_can_delete(self):
+        responses.add(
+            responses.POST,
+            "http://foo.com/webinar/delete?id=ID&host_id=ID&api_key=KEY&api_secret=SECRET",
+        )
         self.component.delete(id="ID", host_id="ID")
-
-        mock_post_request.assert_called_with(
-            "/webinar/delete", params={"id": "ID", "host_id": "ID"}
-        )
 
     def test_requires_id(self):
         with self.assertRaisesRegexp(ValueError, "'id' must be set"):
@@ -41,14 +42,18 @@ class DeleteV1TestCase(unittest.TestCase):
 class DeleteV2TestCase(unittest.TestCase):
     def setUp(self):
         self.component = components.webinar.WebinarComponentV2(
-            base_uri="http://foo.com", config={"api_key": "KEY", "api_secret": "SECRET"}
+            base_uri="http://foo.com",
+            config={
+                "api_key": "KEY",
+                "api_secret": "SECRET",
+                "version": util.API_VERSION_2,
+            },
         )
 
-    @patch.object(components.base.BaseComponent, "delete_request", return_value=True)
-    def test_can_delete(self, mock_delete_request):
-        self.component.delete(id="ID")
-
-        mock_delete_request.assert_called_with("/webinars/ID", params={"id": "ID"})
+    @responses.activate
+    def test_can_delete(self):
+        responses.add(responses.DELETE, "http://foo.com/webinars/42?id=42")
+        self.component.delete(id="42")
 
     def test_requires_id(self):
         with self.assertRaisesRegexp(ValueError, "'id' must be set"):
--- tests/zoomus/components/webinar/test_update.py
+++ tests/zoomus/components/webinar/test_update.py
@@ -1,12 +1,8 @@
-from datetime import datetime
+import datetime
 import unittest
 
-try:
-    from unittest.mock import patch
-except ImportError:
-    from mock import patch
-
-from zoomus import components
+from zoomus import components, util
+import responses
 
 
 def suite():
@@ -19,16 +15,21 @@ def suite():
 class UpdateV1TestCase(unittest.TestCase):
     def setUp(self):
         self.component = components.webinar.WebinarComponent(
-            base_uri="http://foo.com", config={"api_key": "KEY", "api_secret": "SECRET"}
+            base_uri="http://foo.com",
+            config={
+                "api_key": "KEY",
+                "api_secret": "SECRET",
+                "version": util.API_VERSION_1,
+            },
         )
 
-    @patch.object(components.base.BaseComponent, "post_request", return_value=True)
-    def test_can_update(self, mock_post_request):
+    @responses.activate
+    def test_can_update(self):
+        responses.add(
+            responses.POST,
+            "http://foo.com/webinar/update?id=ID&host_id=ID&api_key=KEY&api_secret=SECRET",
+        )
         self.component.update(id="ID", host_id="ID")
-
-        mock_post_request.assert_called_with(
-            "/webinar/update", params={"id": "ID", "host_id": "ID"}
-        )
 
     def test_requires_id(self):
         with self.assertRaisesRegexp(ValueError, "'id' must be set"):
@@ -38,30 +39,32 @@ class UpdateV1TestCase(unittest.TestCase):
         with self.assertRaisesRegexp(ValueError, "'host_id' must be set"):
             self.component.update(id="ID")
 
-    @patch.object(components.base.BaseComponent, "post_request", return_value=True)
-    def test_start_time_gets_transformed(self, mock_post_request):
-        self.component.update(id="42", host_id="HOST", start_time=datetime(1969, 1, 1))
-        mock_post_request.assert_called_with(
-            "/webinar/update",
-            params={
-                "id": "42",
-                "host_id": "HOST",
-                "start_time": "1969-01-01T00:00:00Z",
-            },
+    @responses.activate
+    def test_start_time_gets_transformed(self):
+        start_time = datetime.datetime(1969, 1, 1, 1, 1)
+        responses.add(
+            responses.POST,
+            "http://foo.com/webinar/update?id=42&host_id=HOST"
+            "&start_time=1969-01-01T01%3A01%3A00Z&api_key=KEY&api_secret=SECRET",
         )
+        self.component.update(id="42", host_id="HOST", start_time=start_time)
 
 
 class UpdateV2TestCase(unittest.TestCase):
     def setUp(self):
         self.component = components.webinar.WebinarComponentV2(
-            base_uri="http://foo.com", config={"api_key": "KEY", "api_secret": "SECRET"}
+            base_uri="http://foo.com",
+            config={
+                "api_key": "KEY",
+                "api_secret": "SECRET",
+                "version": util.API_VERSION_1,
+            },
         )
 
-    @patch.object(components.base.BaseComponent, "patch_request", return_value=True)
-    def test_can_update(self, mock_patch_request):
-        self.component.update(id="ID")
-
-        mock_patch_request.assert_called_with("/webinars/ID", params={"id": "ID"})
+    @responses.activate
+    def test_can_update(self):
+        responses.add(responses.PATCH, "http://foo.com/webinars/42?id=42")
+        self.component.update(id="42")
 
     def test_requires_id(self):
         with self.assertRaisesRegexp(ValueError, "'id' must be set"):
