8b54e89614e984cfb43d9c7c597fbc6313f97249
Aarno Aukia <aarno.aukia@vshn.ch>
2020-04-16 23:10:43 +0300

    fix phone tests not being executed

--- zoomus/components/phone.py
+++ zoomus/components/phone.py
@@ -6,39 +6,6 @@ from zoomus import util
 from zoomus.components import base
 
 
-# class UserComponent(base.BaseComponent):
-#     """Component dealing with all user related matters"""
-#
-#     def list(self, **kwargs):
-#         return self.post_request("/user/list", params=kwargs)
-#
-#     def pending(self, **kwargs):
-#         return self.post_request("/user/pending", params=kwargs)
-#
-#     def create(self, **kwargs):
-#         return self.post_request("/user/create", params=kwargs)
-#
-#     def update(self, **kwargs):
-#         util.require_keys(kwargs, "id")
-#         return self.post_request("/user/update", params=kwargs)
-#
-#     def delete(self, **kwargs):
-#         util.require_keys(kwargs, "id")
-#         return self.post_request("/user/delete", params=kwargs)
-#
-#     def cust_create(self, **kwargs):
-#         util.require_keys(kwargs, ["type", "email"])
-#         return self.post_request("/user/custcreate", params=kwargs)
-#
-#     def get(self, **kwargs):
-#         util.require_keys(kwargs, "id")
-#         return self.post_request("/user/get", params=kwargs)
-#
-#     def get_by_email(self, **kwargs):
-#         util.require_keys(kwargs, ["email", "login_type"])
-#         return self.post_request("/user/getbyemail", params=kwargs)
-
-
 class PhoneComponentV2(base.BaseComponent):
     def numbers_list(self, **kwargs):
         return self.get_request("/phone/numbers", params=kwargs)
@@ -57,21 +24,3 @@ class PhoneComponentV2(base.BaseComponent):
 
     def users(self, **kwargs):
         return self.get_request("/phone/users", params=kwargs)
-
-    # def list(self, **kwargs):
-    #     return self.get_request("/users", params=kwargs)
-    #
-    # def create(self, **kwargs):
-    #     return self.post_request("/users", data=kwargs)
-    #
-    # def update(self, **kwargs):
-    #     util.require_keys(kwargs, "id")
-    #     return self.patch_request("/users/{}".format(kwargs.get("id")), data=kwargs)
-    #
-    # def delete(self, **kwargs):
-    #     util.require_keys(kwargs, "id")
-    #     return self.delete_request("/users/{}".format(kwargs.get("id")), params=kwargs)
-    #
-    # def get(self, **kwargs):
-    #     util.require_keys(kwargs, "id")
-    #     return self.get_request("/users/{}".format(kwargs.get("id")), params=kwargs)
