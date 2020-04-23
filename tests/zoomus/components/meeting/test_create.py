60e046aad0d9ac1fea93e0e87517562509230184
Joey Wilhelm <tarkatronic@gmail.com>
2020-04-23 07:01:17 -0400

    Update tests to use responses
    This is to start using https://github.com/getsentry/responses instead of direct mocks for the actual API calls. The big advantage here is that we see what each actual full API call itself looks like, rather than just what the call to the internal methods looks like. Thank you @tarkatronic.

--- tests/zoomus/components/meeting/test_create.py
+++ tests/zoomus/components/meeting/test_create.py
@@ -1,12 +1,8 @@
 import datetime
 import unittest
 
-try:
-    from unittest.mock import patch
-except ImportError:
-    from mock import patch
-
 from zoomus import components, util
+import responses
 
 
 def suite():
@@ -20,20 +16,21 @@ def suite():
 class CreateV1TestCase(unittest.TestCase):
     def setUp(self):
         self.component = components.meeting.MeetingComponent(
-            base_uri="http://foo.com", config={"api_key": "KEY", "api_secret": "SECRET"}
+            base_uri="http://foo.com",
+            config={
+                "api_key": "KEY",
+                "api_secret": "SECRET",
+                "version": util.API_VERSION_1,
+            },
         )
 
+    @responses.activate
     def test_can_create(self):
-        with patch.object(
-            components.base.BaseComponent, "post_request", return_value=True
-        ) as mock_post_request:
-
-            self.component.create(host_id="ID", topic="TOPIC", type="TYPE")
-
-            mock_post_request.assert_called_with(
-                "/meeting/create",
-                params={"host_id": "ID", "topic": "TOPIC", "type": "TYPE"},
-            )
+        responses.add(
+            responses.POST,
+            "http://foo.com/meeting/create?host_id=ID&topic=TOPIC&type=TYPE&api_key=KEY&api_secret=SECRET",
+        )
+        self.component.create(host_id="ID", topic="TOPIC", type="TYPE")
 
     def test_requires_host_id(self):
         with self.assertRaisesRegexp(ValueError, "'host_id' must be set"):
@@ -47,51 +44,53 @@ class CreateV1TestCase(unittest.TestCase):
         with self.assertRaisesRegexp(ValueError, "'type' must be set"):
             self.component.create(host_id="ID", topic="TOPIC")
 
-    @patch.object(components.base.BaseComponent, "post_request", return_value=True)
-    def test_does_convert_startime_to_str_if_datetime(self, mock_post_request):
-        start_time = datetime.datetime.utcnow()
+    @responses.activate
+    def test_does_convert_startime_to_str_if_datetime(self):
+        responses.add(
+            responses.POST,
+            "http://foo.com/meeting/create?host_id=ID&topic=TOPIC&type=TYPE&api_key=KEY&api_secret=SECRET&start_time=2020-01-01T01%3A01%3A00Z",
+        )
+        start_time = datetime.datetime(2020, 1, 1, 1, 1)
         self.component.create(
             host_id="ID", topic="TOPIC", type="TYPE", start_time=start_time
-        )
-
-        mock_post_request.assert_called_with(
-            "/meeting/create",
-            params={
-                "host_id": "ID",
-                "topic": "TOPIC",
-                "type": "TYPE",
-                "start_time": util.date_to_str(start_time),
-            },
         )
 
 
 class CreateV2TestCase(unittest.TestCase):
     def setUp(self):
         self.component = components.meeting.MeetingComponentV2(
-            base_uri="http://foo.com", config={"api_key": "KEY", "api_secret": "SECRET"}
+            base_uri="http://foo.com",
+            config={
+                "api_key": "KEY",
+                "api_secret": "SECRET",
+                "version": util.API_VERSION_2,
+            },
         )
 
-    @patch.object(components.base.BaseComponent, "post_request", return_value=True)
-    def test_can_create(self, mock_post_request):
-        self.component.create(user_id="ID", topic="TOPIC", type="TYPE")
-
-        mock_post_request.assert_called_with(
-            "/users/ID/meetings",
-            data={"user_id": "ID", "topic": "TOPIC", "type": "TYPE"},
+    @responses.activate
+    def test_can_create(self):
+        responses.add(
+            responses.POST, "http://foo.com/users/ID/meetings",
+        )
+        response = self.component.create(user_id="ID", topic="TOPIC", type="TYPE")
+        self.assertEqual(
+            response.request.body, '{"user_id": "ID", "topic": "TOPIC", "type": "TYPE"}'
         )
 
     def test_requires_user_id(self):
         with self.assertRaisesRegexp(ValueError, "'user_id' must be set"):
             self.component.create()
 
-    @patch.object(components.base.BaseComponent, "post_request", return_value=True)
-    def test_does_convert_startime_to_str_if_datetime(self, mock_post_request):
-        start_time = datetime.datetime.utcnow()
-        self.component.create(user_id="ID", start_time=start_time)
-
-        mock_post_request.assert_called_with(
-            "/users/ID/meetings",
-            data={"user_id": "ID", "start_time": util.date_to_str(start_time)},
+    @responses.activate
+    def test_does_convert_startime_to_str_if_datetime(self):
+        responses.add(
+            responses.POST, "http://foo.com/users/ID/meetings",
+        )
+        start_time = datetime.datetime(2020, 1, 1, 1, 1)
+        response = self.component.create(user_id="ID", start_time=start_time)
+        self.assertEqual(
+            response.request.body,
+            '{"user_id": "ID", "start_time": "2020-01-01T01:01:00Z"}',
         )
 
 
