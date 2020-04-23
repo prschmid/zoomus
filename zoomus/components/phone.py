1a3dc3fb438c7954b384df4512273bfae04a52c5
Aarno Aukia <aarno.aukia@vshn.ch>
2020-04-23 14:23:00 +0300

    add usage information from api documentation

--- zoomus/components/phone.py
+++ zoomus/components/phone.py
@@ -17,6 +17,25 @@ class PhoneComponentV2(base.BaseComponent):
         )
 
     def call_logs(self, **kwargs):
+        """
+        Retrieve call logs for an account.
+
+        Scopes: phone:read:admin
+
+        Prerequisite:
+        * Business or Enterprise account
+        * A Zoom Phone license
+        * Account Owner and a  with Zoom Phone Management
+
+        :param page_size: The number of records returned within a single API call,
+        default=30, max=300
+        :param page_number: The current page number of returned records, default=1
+        :param from: Start date from which you would like to get the call logs. The start date should be within past six months.
+        :param to: The end date upto which you would like to get the call logs for.
+        The end date should be within past six months.
+        :param type: The type of the call logs. The value can be either "all" or "missed".
+        :return: request object with json data
+        """
         return self.get_request("/phone/call_logs", params=kwargs)
 
     def calling_plans(self, **kwargs):
