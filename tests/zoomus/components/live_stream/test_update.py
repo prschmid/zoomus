86ea75339a15b839f138033b5aa0f45cecbbfe3f
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2020-04-23 13:43:58 +0200

    Use responses lib in live_stream tests

--- tests/zoomus/components/live_stream/test_update.py
+++ tests/zoomus/components/live_stream/test_update.py
@@ -1,54 +1,55 @@
-from datetime import datetime
 import unittest
 
-try:
-    from unittest.mock import patch
-except ImportError:
-    from mock import patch
+from zoomus import components, util
+import responses
 
-from zoomus import components
+
+def suite():
+    """Define all the tests of the module."""
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(UpdateV2TestCase))
+    return suite
 
 
 class UpdateV2TestCase(unittest.TestCase):
     def setUp(self):
         self.component = components.live_stream.LiveStreamComponentV2(
-            base_uri="http://foo.com", config={"api_key": "KEY", "api_secret": "SECRET"}
-        )
-
-    @patch.object(components.base.BaseComponent, "patch_request", return_value=True)
-    def test_can_update(self, mock_post_request):
-        self.component.update(
-            meeting_id="42", stream_url="https://foo.bar", stream_key="12345"
-        )
-
-        mock_post_request.assert_called_with(
-            "/meetings/42/livestream",
-            data={
-                "meeting_id": "42",
-                "stream_url": "https://foo.bar",
-                "stream_key": "12345",
+            base_uri="http://foo.com",
+            config={
+                "api_key": "KEY",
+                "api_secret": "SECRET",
+                "version": util.API_VERSION_2,
             },
         )
 
-    @patch.object(components.base.BaseComponent, "patch_request", return_value=True)
-    def test_can_update_wildcard(self, mock_post_request):
+    @responses.activate
+    def test_can_update(self):
+        responses.add(responses.PATCH, "http://foo.com/meetings/42/livestream")
+        response = self.component.update(
+            meeting_id="42", stream_url="https://foo.bar", stream_key="12345"
+        )
+        self.assertEqual(
+            response.request.body,
+            '{"meeting_id": "42", "stream_url": "https://foo.bar", "stream_key": "12345"}',
+        )
+
+    @responses.activate
+    def test_can_update_wildcard(self):
+        responses.add(responses.PATCH, "http://foo.com/meetings/42/livestream")
+
         data = {
             "meeting_id": "42",
             "stream_url": "https://foo.bar",
             "stream_key": "12345",
         }
-        self.component.update(**data)
 
-        mock_post_request.assert_called_with(
-            "/meetings/42/livestream",
-            data={
-                "meeting_id": "42",
-                "stream_url": "https://foo.bar",
-                "stream_key": "12345",
-            },
+        response = self.component.update(**data)
+        self.assertEqual(
+            response.request.body,
+            '{"meeting_id": "42", "stream_url": "https://foo.bar", "stream_key": "12345"}',
         )
 
-    def test_requires_id(self):
+    def test_requires_meeting_id(self):
         with self.assertRaises(ValueError) as context:
             self.component.update()
             self.assertEqual(context.exception.message, "'meeting_id' must be set")
--- tests/zoomus/components/live_stream_status/test_update.py
+++ tests/zoomus/components/live_stream_status/test_update.py
@@ -1,56 +1,50 @@
-from datetime import datetime
 import unittest
 
-try:
-    from unittest.mock import patch
-except ImportError:
-    from mock import patch
-
-from zoomus import components
+from zoomus import components, util
+import responses
 
 
 class UpdateV2TestCase(unittest.TestCase):
     def setUp(self):
         self.component = components.live_stream_status.LiveStreamStatusComponentV2(
-            base_uri="http://foo.com", config={"api_key": "KEY", "api_secret": "SECRET"}
+            base_uri="http://foo.com",
+            config={
+                "api_key": "KEY",
+                "api_secret": "SECRET",
+                "version": util.API_VERSION_2,
+            },
         )
 
-    @patch.object(components.base.BaseComponent, "patch_request", return_value=True)
-    def test_can_update(self, mock_post_request):
-        self.component.update(
+    @responses.activate
+    def test_can_update(self):
+        responses.add(responses.PATCH, "http://foo.com/meetings/42/livestream/status")
+        response = self.component.update(
             meeting_id="42",
             action="stop",
             settings={"active_speaker_name": False, "display_name": "inc"},
         )
-
-        mock_post_request.assert_called_with(
-            "/meetings/42/livestream/status",
-            data={
-                "meeting_id": "42",
-                "action": "stop",
-                "settings": {"active_speaker_name": False, "display_name": "inc"},
-            },
+        self.assertEqual(
+            response.request.body,
+            '{"meeting_id": "42", "action": "stop", "settings": {"active_speaker_name": false, "display_name": "inc"}}',
         )
 
-    @patch.object(components.base.BaseComponent, "patch_request", return_value=True)
-    def test_can_update_wildcard(self, mock_post_request):
+    @responses.activate
+    def test_can_update_wildcard(self):
+        responses.add(responses.PATCH, "http://foo.com/meetings/42/livestream/status")
+
         data = {
             "meeting_id": "42",
             "action": "stop",
             "settings": {"active_speaker_name": False, "display_name": "inc"},
         }
-        self.component.update(**data)
 
-        mock_post_request.assert_called_with(
-            "/meetings/42/livestream/status",
-            data={
-                "meeting_id": "42",
-                "action": "stop",
-                "settings": {"active_speaker_name": False, "display_name": "inc"},
-            },
+        response = self.component.update(**data)
+        self.assertEqual(
+            response.request.body,
+            '{"meeting_id": "42", "action": "stop", "settings": {"active_speaker_name": false, "display_name": "inc"}}',
         )
 
-    def test_requires_id(self):
+    def test_requires_meeting_id(self):
         with self.assertRaises(ValueError) as context:
             self.component.update()
             self.assertEqual(context.exception.message, "'meeting_id' must be set")
