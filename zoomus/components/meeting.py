7727ccb4cb5fa532ec33b5e8eb258beed43e938c
Pritam Sukumar <pritam@avanti.in>
2020-07-02 22:20:51 +0530

    [PPS] Changes to PUT request

--- zoomus/components/meeting.py
+++ zoomus/components/meeting.py
@@ -4,6 +4,8 @@ from __future__ import absolute_import
 
 from zoomus import util
 from zoomus.components import base
+import json
+import ast
 
 
 class MeetingComponent(base.BaseComponent):
@@ -39,7 +41,6 @@ class MeetingComponent(base.BaseComponent):
         util.require_keys(kwargs, ["id", "host_id"])
         return self.post_request("/meeting/get", params=kwargs)
 
-
 class MeetingComponentV2(base.BaseComponent):
     def list(self, **kwargs):
         util.require_keys(kwargs, "user_id")
@@ -69,4 +70,47 @@ class MeetingComponentV2(base.BaseComponent):
         util.require_keys(kwargs, "id")
         return self.delete_request(
             "/meetings/{}".format(kwargs.get("id")), params=kwargs
+        )
+
+    def register(self, **kwargs):
+        util.require_keys(kwargs, ["id", "email", "first_name", "last_name", "phone"])
+        
+        params={
+            "email": kwargs["email"],
+            "first_name": kwargs["first_name"],
+            "last_name": kwargs["last_name"],
+            "custom_questions": [
+                {
+                    "title": "Phone Number",
+                    "value": kwargs["phone"]
+                }
+            ]
+        }
+
+        if "city" in kwargs:
+            params["city"] = kwargs["city"]
+
+        return self.post_request(
+            "/meetings/{}/registrants".format(kwargs.get("id")), data=params
+        )
+    
+    def approve(self, **kwargs):
+        util.require_keys(kwargs, ["id", "email", "userid"])
+        params = {
+            "action": "approve",
+            "registrants": [
+                {
+                    "id": kwargs["userid"],
+                    "email": kwargs["email"]
+                }
+            ]
+        }
+        return self.put_request(
+            "/meetings/{}/registrants/status".format(kwargs.get("id")), data=params
+        )
+
+    def get_registration_questions(self, **kwargs):
+        util.require_keys(kwargs, ["id"])
+        return self.get_request(
+            "/meetings/{}/registrants/questions".format(kwargs.get("id"))
         )
