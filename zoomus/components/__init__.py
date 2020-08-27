a3c1fadf17ab4bc16f1cdc00db24abc7ce569985
dhess <dan.hess@kippbayarea.org>
2020-08-27 10:25:00 -0700

    Add group to components

--- zoomus/components/__init__.py
+++ zoomus/components/__init__.py
@@ -2,4 +2,14 @@
 
 from __future__ import absolute_import
 
-from . import meeting, metric, past_meeting, phone, recording, report, user, webinar
+from . import (
+    meeting,
+    metric,
+    past_meeting,
+    phone,
+    recording,
+    report,
+    user,
+    webinar,
+    group,
+)
