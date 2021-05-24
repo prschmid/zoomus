3e38b8ea37a8be8a23325f1b21a8dd22c9742c97
Patrick R. Schmid <prschmid@gmail.com>
2021-05-24 19:32:06 -0400

    Update Dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Zoomus'
-copyright = '2018, Patrick Schmid'
-author = 'Patrick Schmid'
+project = "Zoomus"
+copyright = "2018, Patrick Schmid"
+author = "Patrick Schmid"
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.5'
+release = "1.1.5"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,11 +30,10 @@ release = '1.1.5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -47,9 +46,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
